b319c387802de56e4318f405c71b00b7fa75577f
George Tzinos <geotzinos@gmail.com>
2017-05-07 02:16:23 +0300

    UPDATE: Provider

--- src/providers/Provider.ts
+++ src/providers/Provider.ts
@@ -3,9 +3,12 @@ import { RequestOptions, Headers, Http } from "@angular/http";
 
 export class Provider {
     private _options: RequestOptions;
+    private _domain: string;
     constructor(private _http: Http)
     {
+        this._domain = "http://46.101.247.89:8080";
         this._options = new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json' }) });
     }
 
+    public getDomain(){ return this._domain; }
 }
