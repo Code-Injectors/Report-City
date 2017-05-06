643bbab15683382e86b38b2266a270b6f1eb0564
George Tzinos <geotzinos@gmail.com>
2017-05-06 13:15:57 +0300

    UPDATE: Register page structure and component

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'register-page',
   templateUrl: 'register.html'
 })
 export class RegisterPage {
+  registerForm: FormGroup;
+  constructor(public navCtrl: NavController, private builder: FormBuilder) {
+    this.registerForm = builder.group({
+      'email': ['', Validators.required], //username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')])],
+      'password': ['', Validators.required],
+      'firstname': ['', Validators.required],
+      'lastname': ['', Validators.required]
+    })
+  }
 
-  constructor(public navCtrl: NavController) {
-
+  register() {
+    
   }
 }
