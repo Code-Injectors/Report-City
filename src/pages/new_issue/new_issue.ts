f94e67c8eb7473e1d5a42bb20360bea370462f29
George Tzinos <geotzinos@gmail.com>
2017-05-06 22:25:15 +0300

    FIX: Finally fixed everything (Windows 10)

--- src/pages/new_issue/new_issue.ts
+++ src/pages/new_issue/new_issue.ts
@@ -1,47 +1,71 @@
-import { Camera } from 'ionic-native';
+import {Camera} from '@ionic-native/camera';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Transfer, FileUploadOptions, TransferObject } from '@ionic-native/transfer';
 
 @Component({
   selector: 'new-issues-page',
   templateUrl: './new_issue.html'
 })
 export class CreateIssuePage {
+  private addedImages = [];
   private newIssueForm: FormGroup;
-  constructor(public navCtrl: NavController, private builder: FormBuilder){
+
+  constructor(public navCtrl: NavController, private builder: FormBuilder, private transfer: Transfer, private camera: Camera){
       this.newIssueForm = builder.group({
-      'title': ['', Validators.required], //username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')])],
+      'title': ['', Validators.required], 
       'description': ['', Validators.required]
     })
-    
   }
 
   takePicture() {
-     Camera.getPicture({
-        destinationType: Camera.DestinationType.FILE_URI,
-        sourceType : Camera.PictureSourceType.CAMERA,
+     this.camera.getPicture({
+        destinationType: this.camera.DestinationType.FILE_URI,
+        sourceType : this.camera.PictureSourceType.CAMERA,
         targetWidth: 1000,
         targetHeight: 1000
-    }).then((imageData) => {
-        
+    }).then((imageUrl) => {
+        this.addedImages.push(imageUrl);
     }, (err) => {
         console.log(err);
     });
   }
 
   createIssue(){
-
+    this.sendImages();
   }
 
-  sendImage() {
-   /*
-    var options = new FileUploadOptions();
-    options.fileName = fileURL.substr(fileURL.lastIndexOf('/') + 1);
-    options.mimeType = "multipart/form-data";
+  sendImages() {
 
-    var ft = new FileTransfer();
-    ft.upload(fileURL, encodeURI("http://some.server.com/upload.php"), win, fail, options);
-    */
+    let options = {
+      quality: 100
+    };
+
+    for(let i =0; i< this.addedImages.length; i++)
+    {
+
+        this.camera.getPicture(options).then((imageData) => {
+          // imageData is either a base64 encoded string or a file URI
+          // If it's base64:
+
+        const fileTransfer: TransferObject = this.transfer.create();
+
+        let options1: FileUploadOptions = {
+            fileKey: 'file',
+            fileName: 'name.jpg',
+            headers: {}
+        }
+
+        fileTransfer.upload(this.addedImages[i], 'http://localhost/ionic/upload.php', options1)
+          .then((data) => {
+            // success
+            alert("success");
+          }, (err) => {
+            // error
+            alert("error"+JSON.stringify(err));
+          });
+        });
+    }
   }
 }
