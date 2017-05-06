6e5f88f173e6e0092669da977d0c14bbd817ed0d
George Tzinos <geotzinos@gmail.com>
2017-05-06 15:10:55 +0300

    ADD: Search bar

--- src/pages/issues_list/issues_list.ts
+++ src/pages/issues_list/issues_list.ts
@@ -14,7 +14,7 @@ private reports = [{
   "dislikes": 20,
   "comments": ["fsad","fsadf","fasdf"]
 }, {
-  "title": "report 1",
+  "title": "report 2",
   "category":"category 1",
   "description": "description description description description descriptiondescription",
   "likes": 10,
@@ -26,5 +26,19 @@ private user = {
 }
   constructor(public navCtrl: NavController) {
 
+  }
+
+  getReports(searchEvent: any)
+  {
+    //TODO: Get data from backend
+
+    let searchQuery = searchEvent.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (searchQuery && searchQuery.trim() != '') {
+      this.reports = this.reports.filter((item) => {
+        return (item.title.toLowerCase().indexOf(searchQuery.toLowerCase()) > -1);
+      })
+    }
   }
 }
