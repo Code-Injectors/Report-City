99a6ee7bb2cb8f95046af6dd3b48eb10d8f60215
George Tzinos <geotzinos@gmail.com>
2017-05-06 13:15:42 +0300

    UPDATE: Login page structure and component

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,19 +1,29 @@
 import { RegisterPage } from './../register/register';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'login-page',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-
-  constructor(public navCtrl: NavController) {
-
+  private email = "";
+  private password = "";
+  loginForm: FormGroup;
+  constructor(public navCtrl: NavController, private builder: FormBuilder) {
+    this.loginForm = builder.group({
+      'email': ['', Validators.required], //username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')])],
+      'password': ['', Validators.required]
+    })
   }
 
   login() {
-      
+      if(this.loginForm.valid)
+      {
+        console.log("nekkon");
+      }
   }
 
   register() {
