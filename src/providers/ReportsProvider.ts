aa6da768771671adda0be2770dbdce0673817fd8
George Tzinos <geotzinos@gmail.com>
2017-05-07 06:07:13 +0300

    FIX: Get reports issue

---
+++ src/providers/ReportsProvider.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Provider } from "./Provider";
+import { Observable } from "rxjs/Observable";
+import { Storage } from '@ionic/storage';
+
+@Injectable()
+export class ReportsProvider extends Provider {
+ 
+  constructor(private http: Http, private storage: Storage) {
+      super(http, storage);
+  }
+ 
+  getReports(): Promise<Observable<any>> {
+      return this.get(this.getDomain() + "/reports").then(data => {
+          return data.map(res => res);
+      });
+  }
+}
