5ceac76142e202a7683143ec7362e5cabc504e95
George Tzinos <geotzinos@gmail.com>
2017-05-07 07:23:34 +0300

    ADD: Issues list

--- src/pages/issues_list/issues_list.ts
+++ src/pages/issues_list/issues_list.ts
@@ -4,6 +4,7 @@ import { Component , OnInit} from '@angular/core';
 import { NavController, ModalController } from 'ionic-angular';
 import { FilterModal } from "../../components/shared/filter-modal/filter-modal";
 import { ReportsProvider } from "../../providers/ReportsProvider";
+import * as global from "../../app/globals";
 
 @Component({
   selector: 'issues-list-page',
@@ -32,7 +33,6 @@ export class IssuesListPage implements OnInit{
   getReports(searchEvent: any)
   {
     //TODO: Get data from backend
-
     let searchQuery = searchEvent.target.value;
 
     // if the value is an empty string don't filter the items
@@ -41,6 +41,52 @@ export class IssuesListPage implements OnInit{
         return (item.title.toLowerCase().indexOf(searchQuery.toLowerCase()) > -1);
       })
     }
+  }
+
+  review(user_id, issue_id, voteStatus: boolean)
+  {
+    let hasSameVote = {index: -1, status: false};
+    let hasVote = false;
+    let comment = "";
+    for(let i=0;i<this.reports[issue_id].reviews; i++)
+    {
+      if(this.reports[issue_id].reviews[i].user.id == global.user_id)
+      {
+        comment = this.reports[issue_id].reviews[i].comment;
+        if(this.reports[issue_id].reviews[i].isUpvote == voteStatus)
+        {
+          hasSameVote.status = true;
+        }
+      }
+    }
+
+    if(hasSameVote)
+    {
+      //exeis idi kanei like / dislike
+      return;
+    }
+
+    if(hasVote && !hasSameVote)
+    {
+      //tou allazw to like se dislike kai antistrofa
+      this.reports[issue_id].reviews[hasSameVote.index].isUpvote == voteStatus;
+      return;
+    }
+
+    this.reportsProvider.review(
+      {
+        id: issue_id, 
+        isUpvote: voteStatus,
+        comment: comment,
+        user: {id: user_id}
+      }
+    ).then(success => success.subscribe(success => {
+          
+        },
+        err => {
+          console.log(err);
+        })
+    );
   }
 
   showFilterModal()
