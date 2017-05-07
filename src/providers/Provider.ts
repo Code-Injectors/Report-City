3b3fd976ec67454d3c626e47ef707546b07dd3b9
George Tzinos <geotzinos@gmail.com>
2017-05-07 03:10:11 +0300

    ADD: Public options, domain

--- src/providers/Provider.ts
+++ src/providers/Provider.ts
@@ -2,13 +2,19 @@
 import { RequestOptions, Headers, Http } from "@angular/http";
 
 export class Provider {
+    private _headers: Headers;
     private _options: RequestOptions;
     private _domain: string;
     constructor(private _http: Http)
     {
         this._domain = "http://46.101.247.89:8080";
-        this._options = new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json' }) });
+        this._headers = new Headers();
+        this._headers.append('Content-Type', 'application/json');
+        this._options = new RequestOptions({ headers: this._headers});
     }
 
     public getDomain(){ return this._domain; }
+    
+    public getOptions() {return this._options}
+
 }
