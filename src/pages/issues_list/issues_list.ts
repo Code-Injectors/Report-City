5754c3268afba3b6ecc88079f6f83d664d36181a
George Tzinos <geotzinos@gmail.com>
2017-05-07 08:02:46 +0300

    FIX: Review request flow

--- src/pages/issues_list/issues_list.ts
+++ src/pages/issues_list/issues_list.ts
@@ -43,39 +43,48 @@ export class IssuesListPage implements OnInit{
     }
   }
 
-  review(user_id, issue_id, voteStatus: boolean)
+  review(user_id, issue_index, voteStatus)
   {
     let hasSameVote = {index: -1, status: false};
     let hasVote = false;
     let comment = "";
-    for(let i=0;i<this.reports[issue_id].reviews; i++)
+    for(let i=0;i<this.reports[issue_index].reviews; i++)
     {
-      if(this.reports[issue_id].reviews[i].user.id == global.user_id)
+      if(this.reports[issue_index].reviews[i].user.id == global.user_id)
       {
-        comment = this.reports[issue_id].reviews[i].comment;
-        if(this.reports[issue_id].reviews[i].isUpvote == voteStatus)
+        comment = this.reports[issue_index].reviews[i].comment;
+        if(this.reports[issue_index].reviews[i].isUpvote == voteStatus)
         {
           hasSameVote.status = true;
         }
       }
     }
 
-    if(hasSameVote)
+    if(hasSameVote.status)
     {
       //exeis idi kanei like / dislike
       return;
     }
 
-    if(hasVote && !hasSameVote)
+    if(hasVote && !hasSameVote.status)
     {
       //tou allazw to like se dislike kai antistrofa
-      this.reports[issue_id].reviews[hasSameVote.index].isUpvote == voteStatus;
-      return;
+      this.reports[issue_index].reviews[hasSameVote.index].upvote == voteStatus;
     }
+
+    if(voteStatus) {
+      this.reports[issue_index].upVotes = this.reports[issue_index].upVotes + 1;
+      this.reports[issue_index].upVotes = this.reports[issue_index].downVotes - 1;
+    }
+    else {
+       this.reports[issue_index].upVotes = this.reports[issue_index].downVotes + 1;
+       this.reports[issue_index].upVotes = this.reports[issue_index].upVotes - 1;
+    }
+    
 
     this.reportsProvider.review(
       {
-        id: issue_id, 
+        id:  this.reports[issue_index].id, 
         isUpvote: voteStatus,
         comment: comment,
         user: {id: user_id}
