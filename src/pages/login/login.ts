6edfeb5575441b0e4bdec8c1212a305bbadc8904
George Tzinos <geotzinos@gmail.com>
2017-05-07 07:23:45 +0300

    FIX: Login flow

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,18 +1,20 @@
 import { AuthProvider } from './../../providers/AuthProvider';
-import { IssuesListPage } from './../issues_list/issues_list';
 import { RegisterPage } from './../register/register';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Storage } from '@ionic/storage';
+import * as global from '../../app/globals';
+import { IssuesListPage } from "../issues_list/issues_list";
 
 @Component({
   selector: 'login-page',
-  templateUrl: 'login.html'
+  templateUrl: 'login.html',
 })
 export class LoginPage {
   loginForm: FormGroup;
   constructor(public navCtrl: NavController, private builder: FormBuilder,
-  private authProvider: AuthProvider) {
+  private authProvider: AuthProvider, private storage: Storage) {
     this.loginForm = builder.group({
       'email': ['', Validators.required], //username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')])],
       'password': ['', Validators.required]
@@ -22,12 +24,15 @@ export class LoginPage {
   login() {
       if(this.loginForm.valid)
       {
-        this.authProvider.login(this.loginForm.value.email, this.loginForm.value.password).subscribe(_data => {
-          this.navCtrl.setRoot(IssuesListPage);
+        this.authProvider.login(this.loginForm.value.email, this.loginForm.value.password).subscribe(success => {
+            this.storage.set("token", success.token).then(token => {
+                this.navCtrl.setRoot(IssuesListPage);
+            });
+            global.setUserId(success.userId);
         },
-        _err => {
+        err => {
 
-        })
+        });
       }
   }
 
