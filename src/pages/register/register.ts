6ce33aa73c92e89b27741b77f9b10d2635f98b7f
George Tzinos <geotzinos@gmail.com>
2017-05-07 10:10:47 +0300

    FIX: Register request

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,3 +1,4 @@
+import { AuthProvider } from './../../providers/AuthProvider';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
@@ -8,9 +9,9 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 })
 export class RegisterPage {
   registerForm: FormGroup;
-  constructor(public navCtrl: NavController, private builder: FormBuilder) {
+  constructor(public navCtrl: NavController, private builder: FormBuilder, private authProvider: AuthProvider) {
     this.registerForm = builder.group({
-      'email': ['', Validators.required], //username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')])],
+      'email': ['', Validators.compose([Validators.required, Validators.pattern('(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)')])], //username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')])],
       'password': ['', Validators.required],
       'firstname': ['', Validators.required],
       'lastname': ['', Validators.required]
@@ -18,6 +19,23 @@ export class RegisterPage {
   }
 
   register() {
+    if(this.registerForm.valid)
+    {
+      let data = {
+        "email": this.registerForm.value.email,
+        "password": this.registerForm.value.password,
+        "firstname": this.registerForm.value.firstname,
+        "lastname": this.registerForm.value.lastname
+      }
+      this.authProvider.register(data).subscribe(success => {
+           alert("User created.");
+        },
+        err => {
+          
+        });
+      }else {
+      alert("Please fill all fields.");
+    }
     
   }
 }
--- src/providers/AuthProvider.ts
+++ src/providers/AuthProvider.ts
@@ -12,12 +12,21 @@ export class AuthProvider extends Provider {
       super(http, storage);
   }
  
-  login(email: string, password: string, success_callback?) : Observable<any> {
+  login(email: string, password: string) : Observable<any> {
       let headers = new Headers();
       headers.append('Content-Type', 'application/json');
       let options = new RequestOptions({ headers: headers});
 
      return this.http.post(this.getDomain() + '/auth',{username: email, password: password}, options)
+     .map(res => res.json());
+  }
+
+  register(data: any) : Observable<any> {
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      let options = new RequestOptions({ headers: headers});
+
+     return this.http.post(this.getDomain() + '/auth/register',{data}, options)
      .map(res => res.json());
   }
 }
